3d2046faf95703b32119e3f91d4ec8082720cd98
TestingMyself0 <workspace01001@gmail.com>
2025-12-30 20:57:12 +0300

    init

---
+++ src/main/java/com/yana/catapiintegration/CatApiIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.yana.catapiintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatApiIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatApiIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yana/catapiintegration/CatApiIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yana.catapiintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatApiIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
